4c567ae3df22ae5495c888c1d4549695571860ba
조상호 컴퓨터 <teha007@naver.com>
2021-07-11 22:06:23 +0900

    완료

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="typescript">
+	import type { DrinkType } from '../types';
+
+	export async function load({ fetch }) {
+		const drink: DrinkType = await (await fetch('index.json')).json();
+
+		return {
+			props: { drinkProp: drink }
+		};
+	}
+</script>
+
+<script lang="typescript">
+	export let drinkProp: DrinkType;
+
+	let drinkState: DrinkType = drinkProp;
+
+	const handleOnClick = async () => {
+		const result = await (await fetch('index.json')).json();
+		drinkState = result;
+	};
+</script>
+
+<div class="wrapper">
+	<button on:click={handleOnClick}>Get new drink</button>
+	<h2>{drinkState.name}</h2>
+	<img class="drink-thumb" src={drinkState.thumbUrl} alt="drink-thumb" />
+	<p>{drinkState.instructions}</p>
+
+	{#each drinkState.ingredients as ingredient}
+		<p class="ingredient">{ingredient.amount}{ingredient.name}</p>
+	{/each}
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	.drink-thumb {
+		width: 300px;
+		border-radius: 1rem;
+	}
+
+	p {
+		max-width: 500px;
+		text-align: center;
+	}
+
+	.ingredient {
+		margin: 2px 0;
+	}
+</style>
